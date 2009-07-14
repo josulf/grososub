0739ba023edf7af01e3592507ab95d455bfb60c4
Fregona <Fregona@6b15e7b3-9dda-4132-bfb2-0da4325e0cb2>
2009-07-14 17:52:32 +0000

    ASSTime + Test Cases probak

---
+++ trunk/GrosoSub/Test/ASSTimeTest.h
@@ -0,0 +1,16 @@
+//
+//  ASSTimeTest.h
+//  GrosoSub
+//
+//  Created by Josu López Fernández on 14/07/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface ASSTimeTest : SenTestCase {
+
+}
+
+@end

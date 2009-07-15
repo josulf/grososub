408737175425e464e5d948f3913f9f23c18d5b05
Josu López <josulf@users.noreply.github.com>
2009-07-15 23:22:08 +0000

    ASS fitxategiak ireki eta gorde daitezke (ASSAttatchment falta da)

---
+++ GrosoSub/ASS/ASSColour.h
@@ -0,0 +1,25 @@
+//
+//  ASSColour.h
+//  GrosoSub
+//
+//  Created by Josu López Fernández on 15/07/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ASSColour : NSObject {
+	NSUInteger alpha, red, green, blue;
+}
+
+@property (assign) NSUInteger alpha;
+@property (assign) NSUInteger red;
+@property (assign) NSUInteger green;
+@property (assign) NSUInteger blue;
+
+- (NSString *) description;
+- (void) parseString:(NSString *)aString;
+- (id) initWithString:(NSString *)aString;
+- (id) init;
+@end

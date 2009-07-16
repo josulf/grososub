db1b6f99c564f6bdbae1031e6d8ef77dbbd155d8
Fregona <Fregona@6b15e7b3-9dda-4132-bfb2-0da4325e0cb2>
2009-07-16 15:48:50 +0000

    MyDocument -> ASSScript

---
+++ trunk/GrosoSub/ASSScript.h
@@ -0,0 +1,41 @@
+//
+//  MyDocument.h
+//  GrosoSub
+//
+//  Created by Josu López Fernández on 19/11/08.
+//  Copyright (C) 2008 Josu López Fernández <fregona@fregona.biz>.
+//	All rights reserved.
+//
+//	This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; only version 2 of the License.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License along
+//	with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ASS/ASSHeader.h"
+#import "ASS/ASSStyleList.h"
+#import "ASS/ASSEventList.h"
+
+@interface ASSScript : NSDocument
+{
+	NSString *name;
+	ASSHeader *headers;
+	ASSStyleList *styles;
+	ASSEventList *events;
+}
+
+@property (copy) NSString *name;
+@property (retain) ASSHeader *headers;
+@property (retain) ASSStyleList *styles;
+@property (retain) ASSEventList *events;
+
+@end

1d49ec0f52ba3507e7e8da7e0ceae252e83d2978
Josu López <josulf@users.noreply.github.com>
2009-07-30 23:35:58 +0000

    Aurreko commit-a ez da ondo egin, mugitutako fitxategiak ezabatu. ASSRange klasea gehituta.

---
+++ GrosoSub/ASS/ASSRange.h
@@ -0,0 +1,18 @@
+//
+//  ASSRange.h
+//  GrosoSub
+//
+//  Created by Josu López Fernández on 31/07/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ASSRange : NSObject {
+	NSRange range;
+}
+
+@property (assign) NSRange range;
+
+@end
---
+++ GrosoSub/ASS/ASSRange.m
@@ -0,0 +1,16 @@
+//
+//  ASSRange.m
+//  GrosoSub
+//
+//  Created by Josu López Fernández on 31/07/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "ASSRange.h"
+
+
+@implementation ASSRange
+
+@synthesize range;
+
+@end

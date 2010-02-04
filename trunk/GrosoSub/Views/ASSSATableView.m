0b38eacd28bce16ddafdb28dbf17226579dea217
Fregona <Fregona@6b15e7b3-9dda-4132-bfb2-0da4325e0cb2>
2010-02-04 16:05:29 +0000

    Estilo laguntzailea amaituta :D

--- trunk/GrosoSub/Views/ASSSATableView.m
+++ trunk/GrosoSub/Views/ASSSATableView.m
@@ -3,11 +3,24 @@
 //  GrosoSub
 //
 //  Created by Josu López Fernández on 03/02/10.
-//  Copyright 2010 Euskal Herriko Unibertsitatea. All rights reserved.
+//  Copyright (C) 2010 Josu López Fernández <fregona@fregona.biz>.
+//	All rights reserved.
+//
+//	This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; only version 2 of the License.
+//
+//	This program is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//
+//	You should have received a copy of the GNU General Public License along
+//	with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 
 #import "ASSSATableView.h"
-
 
 @implementation ASSSATableView
 - (void)keyDown:(NSEvent *)theEvent
@@ -36,6 +49,11 @@
 				break;
 		}
 	}
+}
+
+- (void)mouseDown:(NSEvent *)theEvent {
+	[super mouseDown:theEvent];
+    [[NSNotificationCenter defaultCenter] postNotificationName:@"ASSStylingAssistantCommit" object:self];
 }
 
 @end
